7cb1fb84192a93483de435c15c8773c713f437f9
s0hvaperuna <tariqharb54@gmail.com>
2017-08-27 12:21:39 +0300

    removed old commands command

--- cogs/utils.py
+++ cogs/utils.py
@@ -9,26 +9,6 @@ class Utilities(Cog):
     def __init__(self, bot):
         super().__init__(bot)
 
-    @command(name='commands', pass_context=True, ignore_extra=True, enabled=False)
-    async def bot_commands(self, ctx):
-        s = ''
-
-        seen = set()
-        commands = self.bot.commands.values()
-        commands = [seen.add(c.name) or c for c in commands if c.name not in seen]
-        del seen
-        commands = sorted(commands, key=lambda c: c.name)
-
-        for command in commands:
-            try:
-                s += '{}: level {}\n'.format(command.name, command.level)
-            except Exception as e:
-                print('[ERROR] Command info failed. %s' % e)
-
-        s = split_string(s, splitter='\n')
-        for string in s:
-            await self.bot.send_message(ctx.message.author, string)
-
     @command(ignore_extra=True)
     async def ping(self):
         """Ping pong"""

a34dd5d49e3af49b61b4bcf33d22c1e81f87cb23
s0hvaperuna <tariqharb54@gmail.com>
2018-01-06 18:50:40 +0200

    Fix attachments not working in add_emote

--- cogs/server.py
+++ cogs/server.py
@@ -83,7 +83,7 @@ class Server(Cog):
             if not ctx.message.attachments:
                 return await self.bot.say('No image provided')
 
-            data = await self._dl(ctx.message.attachments[0])
+            data = await self._dl(ctx.message.attachments[0].url)
             name = link + ' '.join(name)
             await self.bot.say('What do you want to name the emote as', delete_after=30)
             msg = await self.bot.wait_for_message(author=author,
@@ -153,10 +153,10 @@ class Server(Cog):
                 already_b64 = False
 
             try:
-                 emote = await self.bot.create_custom_emoji(server=server, name=name,
-                                                            image=img,
-                                                            already_b64=already_b64)
-                 emotes.append(emote)
+                emote = await self.bot.create_custom_emoji(server=server, name=name,
+                                                           image=img,
+                                                           already_b64=already_b64)
+                emotes.append(emote)
             except discord.HTTPException as e:
                 if e.code == 400:
                     return await self.bot.say('Emote capacity reached\n{}'.format(e))

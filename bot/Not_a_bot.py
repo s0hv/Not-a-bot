852fafb5d94aceac7a050d885dcaa4bb2dc88cad
s0hvaperuna <tariqharb54@gmail.com>
2018-03-04 00:11:48 +0200

    Rip billy

--- bot/Not_a_bot.py
+++ bot/Not_a_bot.py
@@ -184,9 +184,9 @@ class NotABot(Bot):
 
     async def on_ready(self):
         terminal.info('Logged in as {0.user.name}'.format(self))
-        await self.change_presence(game=discord.Game(name=self.config.game))
         self.dbutil.add_command('help')
         asyncio.ensure_future(self._load_cogs(), loop=self.loop)
+        await self.change_presence(game=discord.Game(name=self.config.game))
         await self.cache_servers()
         if self._random_color is None:
             self._random_color = self.loop.create_task(self._random_color_task())

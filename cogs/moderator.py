5b2986521338db350e2da8758e7f8acacc7b3075
s0hvaperuna <tariqharb54@gmail.com>
2018-06-07 22:09:49 +0300

    fix minutes hours

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -401,7 +401,7 @@ class Moderator(Cog):
         state.add(ctx.author.id)
 
         try:
-            await ctx.send(f'{user.mention} type accept to join this mute roll of {"hours" if use_hours else "minutes"} hours')
+            await ctx.send(f'{user.mention} type accept to join this mute roll of {hours}{"hours" if use_hours else "minutes"}')
 
             _check = basic_check(user, ctx.channel)
 

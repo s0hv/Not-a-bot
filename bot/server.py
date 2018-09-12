59efe3f4f2052bf9fea80f6e44e123d141bc13e8
s0hvaperuna <tariqharb54@gmail.com>
2018-09-12 22:27:02 +0300

    testing server

--- bot/server.py
+++ bot/server.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 
 from sanic import Sanic
+from sanic.response import text
+
 
 logger = logging.getLogger('debug')
 
@@ -16,10 +18,12 @@ class WebhookServer:
         async def webhook(request):
             if request.headers.get('Authorization', None) != self.bot.config.dbl_auth:
                 logger.warning('Unauthorized webhook access')
-                return
+                return text('OK')
 
             for listener in self._listeners:
                 asyncio.ensure_future(listener(request.json), loop=bot.loop)
+
+            return text('OK')
 
         self._server = asyncio.run_coroutine_threadsafe(app.create_server(bot.config.dbl_host,
                                                                           bot.config.dbl_port),

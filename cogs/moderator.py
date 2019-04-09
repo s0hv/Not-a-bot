bc559d6e9c98c4f2102f88e9bdf9e70ccb18ec8f
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2019-04-09 15:57:39 +0300

    fix purge not moving on when no messages were deleted with channel.purge

--- cogs/moderator.py
+++ cogs/moderator.py
@@ -1081,8 +1081,8 @@ class Moderator(Cog):
                 embed = self.purge_embed(ctx, messages, users={'<@!%s>' % user})
                 await self.send_to_modlog(guild, embed=embed)
 
-            await ctx.send(f'Checked the last {max_messages} of the channel {channel} and deleted {len(messages)} messages', delete_after=20)
-            return
+                await ctx.send(f'Checked the last {max_messages} of the channel {channel} and deleted {len(messages)} messages', delete_after=20)
+                return
 
         t = datetime.utcnow() - timedelta(days=14)
         t = datetime2sql(t)

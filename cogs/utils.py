918bdf72a7d7da6701d4e3f9d949b3e48fe94c5b
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2019-06-26 15:14:14 +0300

    only show discord.__version__

--- cogs/utils.py
+++ cogs/utils.py
@@ -13,7 +13,6 @@ from io import StringIO
 from urllib.parse import quote
 
 import discord
-import pkg_resources
 import psutil
 import pytz
 from asyncpg.exceptions import PostgresError
@@ -259,7 +258,7 @@ class Utilities(Cog):
                 i += 1
 
         embed = discord.Embed(title='Stats', colour=random_color())
-        embed.add_field(name='discord.py version', value=f"{discord.__version__} ({pkg_resources.get_distribution('discord.py').version})")
+        embed.add_field(name='discord.py version', value=f"{discord.__version__}")
         embed.add_field(name='Uptime', value=uptime)
         embed.add_field(name='Servers', value=str(guilds))
         embed.add_field(name='Users', value=str(users))

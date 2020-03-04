293b32a8adf53f3246ca007d64b8dd0d6e97e761
s0hvaperuna <s0hvaperuna@gachimuchi.men>
2020-03-04 20:23:24 +0200

    test

---
+++ cogs/r9k.py
@@ -0,0 +1,57 @@
+import asyncio
+
+from cogs.cog import Cog
+from utils import unzalgo
+
+
+class R9K(Cog):
+    def __init__(self, bot):
+        super().__init__(bot)
+        self._messages = []
+        self._update_task = asyncio.run_coroutine_threadsafe(self._update_loop(), loop=bot.loop)
+
+    def cog_unload(self):
+        self._update_task.cancel()
+        try:
+            self._update_task.result(timeout=20)
+        except TimeoutError:
+            pass
+
+    async def _update_loop(self):
+        while True:
+            if not self._messages:
+                await asyncio.sleep(10)
+                continue
+
+            messages = self._messages
+            self._messages = []
+
+            try:
+                sql = 'INSERT INTO r9k (message) VALUES ($1) ON CONFLICT DO NOTHING'
+                await self.bot.dbutil.execute(sql, messages)
+            except:
+                pass
+
+            await asyncio.sleep(10)
+
+    @Cog.listener()
+    async def on_message(self, msg):
+        if msg.channel.id != 297061271205838848:
+            return
+
+        # Don't wanna log bot messages
+        if msg.author.bot:
+            return
+
+        # Gets the content like you see in the client
+        content = msg.clean_content
+
+        # Remove zalgo text
+        content = unzalgo.unzalgo(content)
+        self._messages.append(content)
+        if self._update_task.done():
+            self._update_task = asyncio.run_coroutine_threadsafe(self._update_loop(), loop=self.bot.loop)
+
+
+def setup(bot):
+    bot.add_cog(R9K(bot))
